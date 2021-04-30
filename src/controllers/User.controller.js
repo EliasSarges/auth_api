7c37b3e87513d7b651291123f89e863359ec55fa
Elias Castro <eliassarges@outlook.com>
2021-04-30 12:07:53 -0300

    [Implementação] : criação do cadastro de usuario

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -1,9 +1,46 @@
 const db = require("../database/connection");
 
+const bcrypt = require("bcrypt");
+const { v4: uuid } = require("uuid");
+const jwt = require("jsonwebtoken");
+
 module.exports = {
   async index(req, res) {
     const data = await db("users");
 
     return res.json({ users: data });
+  },
+
+  async create(req, res) {
+    const { first_name, last_name, email, password } = req.body;
+
+    try {
+      if (!first_name || !last_name || !email || !password) {
+        return res
+          .status(500)
+          .json({ erro: "forneça todos os dados necessarios!" });
+      }
+
+      const user = {
+        id: uuid(),
+        first_name,
+        last_name,
+        email,
+        password: await bcrypt.hash(password, 10),
+      };
+
+      await db("users").insert(user);
+      user.password = undefined;
+
+      const token = jwt.sign({ user }, process.env.SECRET);
+
+      return res.header({ Authorization: token }).status(201).json(user);
+    } catch (error) {
+      if (error.errno == 19) {
+        return res.status(500).json({ erro: "email ja cadastrado!" });
+      } else {
+        return res.status(500).json({ erro: "algo deu errado!" });
+      }
+    }
   },
 };
--- src/routes/User/index.js
+++ src/routes/User/index.js
@@ -2,4 +2,5 @@ const UserController = require("../../controllers/User.controller");
 
 module.exports = (routes) => {
   routes.get("/users", UserController.index);
+  routes.post("/users", UserController.create);
 };
