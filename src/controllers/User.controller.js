b3a1ac6259a3fe8cbe6d34634b896774bbe09869
Elias Castro <eliassarges@outlook.com>
2021-04-30 10:33:24 -0300

    [Implementação] : adição da conexão com o banco de dados

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -1,3 +1,5 @@
+const db = require("../database/connection");
+
 module.exports = {
   async index(req, res) {
     res.send("user controller");
---
+++ src/database/connection.js
@@ -0,0 +1,7 @@
+const knex = require("knex");
+const knex_file = require("../../knexfile");
+
+const connection = knex(knex_file.development);
+console.log("banco conectado");
+
+module.exports = connection;
