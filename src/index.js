55674dc84dc1406c5cfb887fec15386509b84ba7
Elias Castro <eliassarges@outlook.com>
2021-04-30 10:23:38 -0300

    [Implementação] : adição do cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+const app = express();
 const routes = require("./routes/routes");
 
-const app = express();
-
+app.use(cors());
+app.use(express.json());
 app.use(routes);
+
 module.exports = app;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const routes = Router();
 
+// importação das rotas
 require("./User")(routes);
 
+// rota fallback
 routes.get("*", (req, res) => {
   res.send("rota nao encontrada");
 });
