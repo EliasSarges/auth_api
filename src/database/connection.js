a349e4a341943c7f4e6002c46d97bfa430e106a1
Elias de Jesus Castro Sarges <elias.castro@cest.edu.br>
2021-05-05 15:35:15 -0300

    [Alteração] : remoção do console.log do server.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,5 +3,4 @@ const knex_file = require("../../knexfile");
 
 const connection = knex(knex_file.development);
 console.log("banco conectado");
-console.log(knex_file.development);
 module.exports = connection;
